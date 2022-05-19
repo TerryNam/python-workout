700108104f5554c605dbe820a938bb1e2091ebc7
Sunghwan Cho <sunghwan@spothero.com>
2022-05-19 16:04:25 -0400

    implemented the second condition.

--- 06.py
+++ 06.py
@@ -6,7 +6,7 @@ def pl_sentence():
         if pl_words[0] in 'aeiou':
             pl_list.append(pl_words + 'way')
         elif pl_words[0] not in 'aeiou':
-            pl_list = pl_words[1::] + pl_words[0] + 'ay'
+            pl_list.append(pl_words[1::] + pl_words[0] + 'ay')
     return " ".join(pl_list)
 
     
