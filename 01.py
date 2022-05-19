645371a1da18845dacd347927b2683ac30d86c6e
Sunghwan Cho <sunghwan@spothero.com>
2022-05-19 16:21:57 -0400

    refomatted 01.py.

--- 01.py
+++ 01.py
@@ -1,17 +1,15 @@
 import random
 
 
-def guessing_game():
+if __name__ == "__main__":
     number = random.randint(0,100)
 
     while True:
      guess_num = int(input('Enter thoughts:')) 
 
-     if guess_num==number:
+     if guess_num == number:
         break
-     elif guess_num<number:
+     elif guess_num < number:
            print(f'Too low') 
      else :
            print(f'Too High')
-
-guessing_game()
