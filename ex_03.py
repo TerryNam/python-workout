7e979fa9397099ca281ab9a5a4177dd78cb97584
TerryNam <103907858+TerryNam@users.noreply.github.com>
2022-05-20 14:58:23 -0400

    ex03 update

--- ex_03.py
+++ ex_03.py
@@ -1,18 +1,18 @@
 def run_timing():
-    total_run_min = 0
-    average_time = 0
+    number_of_runs = 0
+    total_time = 0
     
 
     while True:
-     try:
-         run_min = float(input('Enter 10km run time:'))
-         total_run_min += run_min
-         average_time = total_run_min // ##달린 횟수를 어찌 카운트해야할지..?
-     
-     
-     except:
-         not run_min
-         print(f'Average of'+{} +'times run' +',' + 'over {run_mins} runs')
+        timing = input('Enter 10km run time:').strip() #input 까지 ()로 감싸서, timimg value때문에 print문에서 오류 발생 
+
+        if not timing:
+            break
+
+        number_of_runs +=1
+        total_time += float(timing)
+
+    print(f'Average of 10km run is {total_time/number_of_runs}mins')
 
 run_timing()
 
