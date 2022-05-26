9a035aeed0b83499a97663b653a94606510d136a
TerryNam <terrynam0502@gmail.com>
2022-05-26 01:09:25 -0400

    ex_16 update

--- ex_16.py
+++ ex_16.py
@@ -5,3 +5,15 @@ def dictdiff(first, second):
     for key in all_keys:
         if first.get(key) != second.get(key):
             output[key] = [first.get(key), second.get(key)]
+        elif first.keys() != second.keys():
+            output[key] = []
+    print(output) 
+
+        
+            
+if __name__ == "__main__":
+    first = {'a':1, 'b':2, 'c':4}
+    second = {'a':1, 'b':4, 'c':8}
+    dictdiff(first,second)
+
+         
