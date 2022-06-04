256f1797ff7e17ba9c006561e2fa277c1a22c8de
TerryNam <103907858+TerryNam@users.noreply.github.com>
2022-06-04 13:42:19 -0400

    ex_20 update

--- ex_19.py
+++ ex_19.py
@@ -9,6 +9,7 @@ def passwd_to_dict(filename):
                 user_info = line.split(":")      # str(line)에서 원하는 부분만 뽑아내기 위해 split()메서드로 구분자(:)에 따라 쪼갬
                 users[user_info[0]] = int(user_info[2])  #dict에 keyword,value 선언
     return users
+    
 
 print(passwd_to_dict('passwd.txt'))
 
---
+++ ex_20.py
@@ -0,0 +1,25 @@
+from fileinput import filename
+
+
+def file_count(filename):
+    counts = {'characters': 0,
+               'words' : 0,
+               'lines' : 0}
+    unique_words = set()
+
+
+    for one_line in open(filename):
+        counts['lines'] += 1
+        counts['characters'] += len(one_line)
+        counts['words'] += len(one_line.split())
+
+        unique_words.update(one_line.split())
+    counts['unique_words'] = len(unique_words)
+
+    for key, value in counts.items():
+        print(f'{key}: {value}')
+
+
+if __name__ == "__main__":
+    
+    file_count('wcfile.txt')    
